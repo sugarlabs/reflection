7d3534909eeead1707eff50f53e0efeac290bd07
Walter Bender <walter@sugarlabs.org>
2015-02-14 17:22:43 -0500

    add category

--- ReflectionActivity.py
+++ ReflectionActivity.py
@@ -14,15 +14,9 @@
 from gi.repository import Gtk,Gdk
 from sugar3.activity import activity
 from sugar3 import profile
-try:
-    from sugar3.graphics.toolbarbox import ToolbarBox
-    _have_toolbox = True
-except ImportError:
-    _have_toolbox = False
-
-if _have_toolbox:
-    from sugar3.activity.widgets import ActivityToolbarButton
-    from sugar3.activity.widgets import StopButton
+from sugar3.graphics.toolbarbox import ToolbarBox
+from sugar3.activity.widgets import ActivityToolbarButton
+from sugar3.activity.widgets import StopButton
 
 from toolbar_utils import button_factory, label_factory, separator_factory, \
                           radio_factory
@@ -49,10 +43,10 @@ PATH = '/org/augarlabs/ReflectionActivity'
 
 
 class ReflectionActivity(activity.Activity):
-    """ Reflection puzzle game """
+    ''' Reflection puzzle game '''
 
     def __init__(self, handle):
-        """ Initialize the toolbars and the game board """
+        ''' Initialize the toolbars and the game board '''
         try:
             super(ReflectionActivity, self).__init__(handle)
         except dbus.exceptions.DBusException, e:
@@ -64,7 +58,7 @@ class ReflectionActivity(activity.Activity):
         else:
             self.colors = ['#A0FFA0', '#FF8080']
 
-        self._setup_toolbars(_have_toolbox)
+        self._setup_toolbars()
         self._setup_dispatch_table()
 
         # Create a canvas
@@ -83,33 +77,21 @@ class ReflectionActivity(activity.Activity):
         else:
             self._game.new_game('horizontal')
 
-    def _setup_toolbars(self, have_toolbox):
-        """ Setup the toolbars. """
+    def _setup_toolbars(self):
+        ''' Setup the toolbars. '''
 
         self.max_participants = 4
 
-        if have_toolbox:
-            toolbox = ToolbarBox()
+        toolbox = ToolbarBox()
 
-            # Activity toolbar
-            activity_button = ActivityToolbarButton(self)
+        activity_button = ActivityToolbarButton(self)
 
-            toolbox.toolbar.insert(activity_button, 0)
-            activity_button.show()
+        toolbox.toolbar.insert(activity_button, 0)
+        activity_button.show()
 
-            self.set_toolbar_box(toolbox)
-            toolbox.show()
-            self.toolbar = toolbox.toolbar
-
-        else:
-            # Use pre-0.86 toolbar design
-            games_toolbar = Gtk.Toolbar()
-            toolbox = activity.ActivityToolbox(self)
-            self.set_toolbox(toolbox)
-            toolbox.add_toolbar(_('Game'), games_toolbar)
-            toolbox.show()
-            toolbox.set_current_toolbar(1)
-            self.toolbar = games_toolbar
+        self.set_toolbar_box(toolbox)
+        toolbox.show()
+        self.toolbar = toolbox.toolbar
 
         my_colors = radio_factory(
             'my-colors', self.toolbar, self._my_colors_cb, group=None)
@@ -134,21 +116,18 @@ class ReflectionActivity(activity.Activity):
 
         self.status = label_factory(self.toolbar, '')
 
-        if _have_toolbox:
-            separator_factory(toolbox.toolbar, False, True)
+        separator_factory(toolbox.toolbar, False, True)
 
         self.robot_button = button_factory(
             'robot-off', self.toolbar, self._robot_cb,
             tooltip= _('Play with the robot.'))
 
-        if _have_toolbox:
-            separator_factory(toolbox.toolbar, True, False)
+        separator_factory(toolbox.toolbar, True, False)
 
-        if _have_toolbox:
-            stop_button = StopButton(self)
-            stop_button.props.accelerator = '<Ctrl>q'
-            toolbox.toolbar.insert(stop_button, -1)
-            stop_button.show()
+        stop_button = StopButton(self)
+        stop_button.props.accelerator = '<Ctrl>q'
+        toolbox.toolbar.insert(stop_button, -1)
+        stop_button.show()
 
     def _my_colors_cb(self, button=None):
         if hasattr(self, '_game'):
@@ -177,7 +156,7 @@ class ReflectionActivity(activity.Activity):
         self.robot_button.set_icon_name(icon)
 
     def write_file(self, file_path):
-        """ Write the grid status to the Journal """
+        ''' Write the grid status to the Journal '''
         [dot_list, orientation] = self._game.save_game()
         self.metadata['orientation'] = orientation
         self.metadata['dotlist'] = ''
@@ -187,7 +166,7 @@ class ReflectionActivity(activity.Activity):
                 self.metadata['dotlist'] += ' '
 
     def _restore(self):
-        """ Restore the game state from metadata """
+        ''' Restore the game state from metadata '''
         if 'orientation' in self.metadata:
             orientation = self.metadata['orientation']
         else:
@@ -202,29 +181,29 @@ class ReflectionActivity(activity.Activity):
     # Collaboration-related methods
 
     def _setup_presence_service(self):
-        """ Setup the Presence Service. """
+        ''' Setup the Presence Service. '''
         self.pservice = presenceservice.get_instance()
         self.initiating = None  # sharing (True) or joining (False)
 
         owner = self.pservice.get_owner()
         self.owner = owner
-        self._share = ""
+        self._share = ''
         self.connect('shared', self._shared_cb)
         self.connect('joined', self._joined_cb)
 
     def _shared_cb(self, activity):
-        """ Either set up initial share..."""
+        ''' Either set up initial share...'''
         self._new_tube_common(True)
 
     def _joined_cb(self, activity):
-        """ ...or join an exisiting share. """
+        ''' ...or join an exisiting share. '''
         self._new_tube_common(False)
 
     def _new_tube_common(self, sharer):
-        """ Joining and sharing are mostly the same... """
+        ''' Joining and sharing are mostly the same... '''
         if self._shared_activity is None:
-            _logger.debug("Error: Failed to share or join activity ... \
-                _shared_activity is null in _shared_cb()")
+            _logger.debug('Error: Failed to share or join activity ... \
+                _shared_activity is null in _shared_cb()')
             return
 
         self.initiating = sharer
@@ -249,16 +228,16 @@ class ReflectionActivity(activity.Activity):
         self._game.set_sharing(True)
 
     def _list_tubes_reply_cb(self, tubes):
-        """ Reply to a list request. """
+        ''' Reply to a list request. '''
         for tube_info in tubes:
             self._new_tube_cb(*tube_info)
 
     def _list_tubes_error_cb(self, e):
-        """ Log errors. """
+        ''' Log errors. '''
         _logger.debug('Error: ListTubes() failed: %s' % (e))
 
     def _new_tube_cb(self, id, initiator, type, service, params, state):
-        """ Create a new tube. """
+        ''' Create a new tube. '''
         _logger.debug('New tube: ID=%d initator=%d type=%d service=%s \
 params=%r state=%d' % (id, initiator, type, service, params, state))
 
@@ -311,13 +290,13 @@ params=%r state=%d' % (id, initiator, type, service, params, state))
         self._game.remote_button_press(dot, color)
 
     def send_event(self, entry):
-        """ Send event through the tube. """
+        ''' Send event through the tube. '''
         if hasattr(self, 'chattube') and self.chattube is not None:
             self.chattube.SendText(entry)
 
 
 class ChatTube(ExportedGObject):
-    """ Class for setting up tube for sharing """
+    ''' Class for setting up tube for sharing '''
 
     def __init__(self, tube, is_initiator, stack_received_cb):
         super(ChatTube, self).__init__(tube, PATH)
